1164a157c5bcb8c0aafc06bf6568db503b616ce8
animesh_shakya <animesh078@academiacollege.edu.np>
2026-03-24 10:02:17 +0545

    Initial commit

---
+++ routes/update.php
@@ -0,0 +1,22 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Install Routes
+|--------------------------------------------------------------------------
+|
+| This route is responsible for handling the intallation process
+|
+|
+|
+*/
+
+use App\Http\Controllers\InstallController;
+use App\Http\Controllers\UpdateController;
+use Illuminate\Support\Facades\Route;
+
+Route::any('/', [UpdateController::class, 'update_software_index'])->name('index');
+Route::any('update-system', [UpdateController::class, 'update_software'])->name('update-system');
+
+Route::fallback(function () {
+    return redirect('/');
+});
